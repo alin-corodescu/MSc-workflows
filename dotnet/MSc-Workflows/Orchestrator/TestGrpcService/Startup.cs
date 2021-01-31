a5b3f8b69ce0cdf238d76b34e77a39cb8c1aacc3
Alin Corodescu <alin.corodescu@gmail.com>
2021-01-31 17:58:30 +0100

    moved the last proto file and added configuration support in the test project

--- dotnet/MSc-Workflows/Orchestrator/TestGrpcService/Startup.cs
+++ dotnet/MSc-Workflows/Orchestrator/TestGrpcService/Startup.cs
@@ -14,12 +14,20 @@ namespace TestGrpcService
 {
     public class Startup
     {
-        public IConfiguration Configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+            
+            Console.WriteLine($"Config: {Configuration["Test"]}");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
